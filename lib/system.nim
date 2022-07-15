04ab6fb18648370f81ec004ab3e64c9ce443232e
EmilIvanichkovv <emil.ivanichkov@gmail.com>
2022-07-15 14:17:28 +0300

    Improve `--nimNoLibc` support
    Add `nimMoveMem` proc
    Add more guards for `--nimNoLibc`

--- lib/system.nim
+++ lib/system.nim
@@ -1766,7 +1766,8 @@ proc compiles*(x: untyped): bool {.magic: "Compiles", noSideEffect, compileTime.
   discard
 
 when notJSnotNims:
-  import system/ansi_c
+  when not defined(nimNoLibc):
+    import system/ansi_c
   import system/memory
 
 
@@ -2236,7 +2237,7 @@ when notJSnotNims:
     when declared(memTrackerOp):
       memTrackerOp("copyMem", dest, size)
   proc moveMem(dest, source: pointer, size: Natural) =
-    c_memmove(dest, source, csize_t(size))
+    discard nimMoveMem(dest, source, size)
     when declared(memTrackerOp):
       memTrackerOp("moveMem", dest, size)
   proc equalMem(a, b: pointer, size: Natural): bool =
@@ -2291,7 +2292,7 @@ when not defined(js) and declared(alloc0) and declared(dealloc):
       inc(i)
     dealloc(a)
 
-when notJSnotNims:
+when notJSnotNims and not defined(nimNoLibc):
   type
     PSafePoint = ptr TSafePoint
     TSafePoint {.compilerproc, final.} = object
